31c2cf62ae2fa7b5e7e2eff8dc8391b9b6fd95f2
dzcdzc124 <dengzhengcai@vivo.com.cn>
2015-07-10 14:59:19 +0800

    no message

---
+++ git_hook_pull.php
@@ -0,0 +1,10 @@
+<?php  
+    $www_folder = "/var/www/html/Maplestory";  
+      
+    //在这里获取到了用户提交的内容, 以及提交者等等, 可以记录到数据库中供以后使用  
+    $raw_json = file_get_contents('php://input');  
+    print_r(json_decode($raw_json, true));  
+      
+    echo shell_exec(" cd $www_folder ; git pull 2>&1");  
+
+?>
