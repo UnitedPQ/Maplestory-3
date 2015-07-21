9d8695607f9250d6d21db0ce3cdf81cde61f51f9
dzcdzc124 <dengzhengcai@vivo.com.cn>
2015-07-21 17:34:18 +0800

    no message

--- git_hook_pull.php
+++ git_hook_pull.php
@@ -21,13 +21,13 @@
 	pclose($handle);
 
 
-    header("Content-type: text/html; charset=utf-8");
+    @header("Content-type: text/html; charset=utf-8");
     /********************
     1、写入内容到文件,追加内容到文件
     2、打开并读取文件内容
     ********************/
     $file  = 'git.log';//要写入文件的文件名（可以是任意文件名），如果文件不存在，将会创建一个
-    $content = "git pull:".date("Y-m-d H:i:s")."/r/n/r/n";
+    $content = "git pull:".@date("Y-m-d H:i:s")."\r\n\r\n";
 
     if($f  = file_put_contents($file, $content,FILE_APPEND)){// 这个函数支持版本(PHP 5) 
         echo "写入成功。<br />";
