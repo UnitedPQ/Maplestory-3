e29969702981d33d332228afe096c7846a72614d
dzcdzc124 <dengzhengcai@vivo.com.cn>
2015-07-22 15:52:16 +0800

    no message

--- git_hook_pull.php
+++ git_hook_pull.php
@@ -1,16 +1,18 @@
 <?php  
-/*    $www_folder = "/var/www/html/Maplestory"; 
-    $git = "/usr/bin/git"; 
+    //三分钟
+    set_time_limit(180);
+    ini_set("memory_limit", "150M");
+    $www_folder = "/var/www/html/Maplestory"; 
+    //$git = "/usr/bin/git"; 
       
     //在这里获取到了用户提交的内容, 以及提交者等等, 可以记录到数据库中供以后使用  
     $raw_json = file_get_contents('php://input');  
     print_r(json_decode($raw_json, true));  
       
-    echo shell_exec("cd $www_folder ; whoami ; $git pull 2>&1");  
-*/
-    //三分钟
-    set_time_limit(180);
-    ini_set("memory_limit", "150M");
+    echo shell_exec("cd $www_folder ; whoami ; git pull 2>&1 ; yes");  
+
+    
+/*
     error_reporting ( E_ALL );
 
 	$dir = '/var/www/html/Maplestory/';//该目录为git检出目录
@@ -22,7 +24,7 @@
 	printf($read);
     unset($read);
 	pclose($handle);
-
+*/
 
     @header("Content-type: text/html; charset=utf-8");
     /********************
